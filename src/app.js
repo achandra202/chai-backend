dcdbaef3ec1a5e0dbec92e11d7ba17510b3f0014
Abhishek <gajab202@gmail.com>
2025-08-28 22:34:05 +0530

    Video and User model Access token

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3000", // Update with your frontend URL   
+    origin: process.env.CORS_ORIGIN, // Update with your frontend URL   
     credentials: true
 }));
 
