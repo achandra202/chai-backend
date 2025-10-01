2b266798533dfa156b33ef8b0cd0f4edf86fc4d1
Abhishek <gajab202@gmail.com>
2025-10-01 23:48:56 +0530

    updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,35 @@
 import asyncHandler from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res, next) => {
-    // Registration logic here
-    res.status(201).json({ message: "User registered successfully" });
+    // // Registration logic here
+    // res.status(201).json({ message: "User registered successfully" });
+    // get user data from req.body
+
+    //get user details from front end
+    //basic validation
+    //check if user already exists
+    //check for image file
+    //hash password
+    //save user to db
+    //remove temp image
+    //remove password and refresh token from user object
+    //check user creation
+
+    //send response
+
+    const { username, email, password } = req.body; 
+
+    //get useer data from frontend
+    console.log(email);
+    console.log(req.file); // if you're uploading a file
+
+    // Basic validation
+    if (!username || !email || !password) {
+        return res.status(400).json({ message: "All fields i.e. username, email, password are required" });
+    }   
+
+    // Simulate user registration (e.g., save to database)
+
 });
 
 const loginUser = asyncHandler(async (req, res, next) => {
