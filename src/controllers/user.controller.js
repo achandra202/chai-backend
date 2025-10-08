551ef890ec08cc53f4339a5b63acb32b732307d4
achandra202 <gajab202@gmail.com>
2025-10-08 10:52:56 +0530

    ok

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,23 @@
+import e from "express";
 import asyncHandler from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res, next) => {
     // Registration logic here
     res.status(201).json({ message: "User registered successfully" });
+
+    //get user details from frontend
+    //validate user details
+    //check if user already exists using email and username
+    //check for image and check for avatar
+    //upload image to cloudinary, avatar 
+    //create user object- create a new user in database
+    //remove password and refreshtoken from user object before sending response
+    //check if user is created successfully
+    //send response
+
+    const { fullName,username, email, password } = req.body
+    console.log("email: ", email);    
+           
 });
 
 const loginUser = asyncHandler(async (req, res, next) => {
